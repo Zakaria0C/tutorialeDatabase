5b40d36b9419ee5c93a8c05e5700346f658235f5
Zakaria0C <zakaria.elmajdoubi707@gmail.com>
2023-06-13 03:45:24 +0100

    ajout de database oracle

---
+++ Database.sql
@@ -0,0 +1,19 @@
+desc student;
+//Alter table myfirsttable add emp_id number(10);
+//Alter table myfirsttable modify emp_id varchar(20);
+//Alter table myfirsttable rename  column emp_id to employee_id ;
+//Alter table myfirsttable drop column EMPLOYEE_ID ;
+//rename Myfirsttable to student ;
+//drop table tablename;
+//drop table student;
+
+create table Myfirsttable
+(
+
+emp_name varchar2(10),
+gender char(10),
+sal number(10),
+DOB date );
+
+
+select*from  Myfirsttable;
